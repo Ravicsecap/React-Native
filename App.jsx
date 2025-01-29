f7ee0ad9f0500f388bcda00e509f55f5dad6afc1
Ravicsecap <upsccse1996@gmail.com>
2025-01-29 13:09:25 +0530

    View , Text ,Button Component

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import {View, Text, Button} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>
+        Namste , Ravi Sahani You are learning New Technology like React Native.
+      </Text>
+
+      <Text style={{fontSize: 30}}>
+        Namste , Ravi Sahani You are learning New Technology like React Native.
+      </Text>
+      <Button title="press" />
+    </View>
+  );
+};
+
+export default App;
