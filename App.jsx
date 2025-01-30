3fb245b76addb3fe3a8ae2ebb1267d42fd206e2e
Ravicsecap <upsccse1996@gmail.com>
2025-01-30 11:23:06 +0530

    Functional Component

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import {View, Text, Button} from 'react-native';
 import React from 'react';
+import FunctionalBasedCom from './src/component/FunctionalBasedCom';
 
 const App = () => {
   return (
@@ -7,10 +8,12 @@ const App = () => {
       <Text style={{fontSize: 30}}>
         Namste , Ravi Sahani You are learning New Technology like React Native.
       </Text>
-
       <Text style={{fontSize: 30}}>
         Namste , Ravi Sahani You are learning New Technology like React Native.
       </Text>
+      <FunctionalBasedCom />
+      <FunctionalBasedCom /> resuablity of ex.
+      <FunctionalBasedCom /> // reuseblity of ex.
       <Button title="press" />
     </View>
   );
---
+++ src/component/FunctionalBasedCom.jsx
@@ -0,0 +1,17 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+// Functional based Component First letter Caps
+// Component is a show on UI is all things are components.
+// if same module import the multiple part then use curly brase {}.
+// component create for resuablity. WRITE ONE TIME USE ANYWHERE.
+
+const FunctionalBasedCom = () => {
+  return (
+    <View>
+      <Text>FunctionalBasedCom</Text>
+    </View>
+  );
+};
+
+export default FunctionalBasedCom;
